cd96d88e8051add5bd9ba6e0ff83faab07134aa4
thetinybug <eoittm@gmail.com>
2019-11-13 17:42:23 +0700

    add receive notf

--- app/src/main/java/com/example/firebasenotfapp/MainActivity.java
+++ app/src/main/java/com/example/firebasenotfapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.firebasenotfapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    FirebaseInstanceId.getInstance().getInstanceId()
+//        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//        @Override
+//        public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//            if (!task.isSuccessful()) {
+//                Log.w(TAG, "getInstanceId failed", task.getException());
+//                return;
+//            }
+//
+//            // Get new Instance ID token
+//            String token = task.getResult().getToken();
+//
+//            // Log and toast
+//            String msg = getString(R.string.msg_token_fmt, token);
+//            Log.d(TAG, msg);
+//            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        }
+//    });
 }
---
+++ app/src/main/java/com/example/firebasenotfapp/NotificationService.java
@@ -0,0 +1,42 @@
+package com.example.firebasenotfapp;
+
+import android.app.Notification;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class NotificationService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+        manager.notify(123, notification);
+    }
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        // Méo biết code sao cai ham nay :v
+        // sendRegistrationToServer(token);
+    }
+}
